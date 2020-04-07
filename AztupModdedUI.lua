eb307df8501d49d79c0dbe868c8ba47ed7f57a50
1337xyz <61508291+1337xyz@users.noreply.github.com>
2020-04-07 09:58:52 +0500

    Update AztupModdedUI.lua

--- AztupModdedUI.lua
+++ AztupModdedUI.lua
@@ -114,7 +114,7 @@ local function initLibrary()
 	UserInputService.InputBegan:Connect(function(Input, gameProcessedEvent)
     	if gameProcessedEvent then return end
 
-    	if Input.KeyCode == Enum.KeyCode.RightShift then
+    	if Input.KeyCode == Enum.KeyCode.RightControl then
         	gui.Enabled = not gui.Enabled
     	end
 	end)
